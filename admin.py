4e8beaced7c77ea04e76aeb967d610a13bf538bd
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2021-02-23 11:48:13 -0600

    Add files via upload

--- admin.py
+++ admin.py
@@ -5,14 +5,18 @@ from io import open
 
 ss = openpyxl.load_workbook('prueba.xlsx')
 sheet = ss.get_sheet_by_name('Inventario')
-ingredientes = sheet['F2':'F24'].value
+# print(sheet.cell(row=1, column=5).value)
+hola = 0
+for i in range(1, 25):
+    if sheet.cell(row = i, column = 5).value == 0:
+        print(sheet.cell(row = i, column = 1).value)
 
 root = Tk()
 root.title('Moras Dulces')
 
-Label(root, text="Ingredientes Moras", fg="darkblue", font=("Times New Roman",28,"bold italic")).pack()
+Label(root, text="Inventario Moras", fg="darkblue", font=("Times New Roman",28,"bold italic")).pack()
 
-Label(root, text=ingredientes, fg="black", font=("Times New Roman", 20,"bold italic")).pack()
+Label(root, text="Ingredientes", fg="black", font=("Times New Roman", 20,"bold italic")).pack()
 mensaje = StringVar()
 mensaje.set("Bienvenida Dolores")
 monitor = Label(root,textvar = mensaje, justify='left')
